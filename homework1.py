c76be8c6c738bfb9a195fa8a4d6766b44139ec96
Ezgiiee <62596880+Ezgiiee@users.noreply.github.com>
2020-12-23 21:28:10 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,22 @@
+
+print("Homework for Day 1")
+
+num1 = int(input("Enter a number :"))
+num2 = int(input("Enter a number :"))
+num3 = int(input("Enter a number :"))
+num4 = int(input("Enter a number :"))
+num5 = int(input("Enter a number :"))
+
+print("The entered first number : {}".format(num1))
+print(f"The entered second number : {num2}")
+print("The entered third number : {}".format(num3))
+print(f"The entered fourth number : {num4}")
+print("The entered fifth number : {}".format(num5))
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(num4))
+print(type(num5))
+
+
